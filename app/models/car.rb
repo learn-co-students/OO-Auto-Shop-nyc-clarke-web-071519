e9b37023fc051a3f8e8cfa6be879a02b550ca019
Valentino Wong <valentino.wong@gmail.com>
2019-07-22 17:53:58 -0400

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,31 @@
 class Car
 
-  attr_reader :make, :model
+  attr_accessor :car_owner, :mechanic
+  attr_reader :make, :model, :classification
+
+  @@all = []
 
   def initialize(make, model, classification)
     @make = make
     @model = model
     @classification = classification
+
+    @@all << self
+  end
+
+  # .all - Get a list of all cars
+  def self.all
+    @@all
+  end
+
+  # .classifications - Get a list of all car classifications
+  def self.classifications
+    self.all.map {|car| car.classification}.uniq
+  end
+
+  # #possible_mechanics - Get a list of mechanics that have an expertise that matches the car classification
+  def possible_mechanics
+    Mechanic.all.select {|mechanic| mechanic.specialty == self.classification}
   end
 
 end
